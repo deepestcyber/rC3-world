1249480fadf4da7abb6dab4d949214c18d476af0
nemo <git@ningu.net>
2020-12-26 01:16:23 +0100

    Test new tiles

---
+++ tilesets/14_Basement_32x32.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="14_Basement_32x32" tilewidth="32" tileheight="32" tilecount="784" columns="16">
+ <image source="Modern_Interiors/1_Tilesets/32x32/Theme_Sorter_32x32/14_Basement_32x32.png" width="512" height="1568"/>
+</tileset>
