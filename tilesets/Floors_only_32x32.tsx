fe7b31e21786921ff5796804f9645aa3a7bada9b
nemo <git@ningu.net>
2020-12-26 01:48:02 +0100

    Do *not* use exported tilesets
    we need to include them into the map at all times

--- tilesets/Floors_only_32x32.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.3" name="Floors_only_32x32" tilewidth="32" tileheight="32" tilecount="216" columns="8">
- <image source="Modern_Interiors/1_Tilesets/32x32/Floors_only_32x32.png" width="256" height="864"/>
-</tileset>
--- tilesets/shadowset32.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.3" name="shadowset32" tilewidth="32" tileheight="32" tilecount="1728" columns="48">
- <image source="shadows/shadowset32x32.png" width="1536" height="1152"/>
-</tileset>
