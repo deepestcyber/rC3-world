52c459aff14fd9f0c1843eb32869f2f2ea0dcb72
merelyrey <merelyrey@gmail.com>
2020-12-29 18:55:41 +0900

    added Japanese garden

---
+++ tilesets/JapZenGarden/Tileset_JapZenGardenA_32x32.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="Tileset_JapZenGardenA_32x32" tilewidth="32" tileheight="32" tilecount="896" columns="56">
+ <image source="Main_Tilemap.png" width="1792" height="512"/>
+</tileset>
